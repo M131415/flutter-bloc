990bab8b2a9419a974595bc66a89d0f3f3d3b8f1
Mauricio Medina <mau172001mece@gmail.com>
2024-05-04 15:46:24 -0600

    Implementando un cubit simple para mostrar nombres aleatorios

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
-import 'package:flutter/material.dart';
 import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const CubitsProvider());
+}
+
+class CubitsProvider extends StatelessWidget {
+  const CubitsProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => UsernameCubit(),)
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/presentation/screens/01_simple_cubit/cubit_screen.dart
+++ lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -1,4 +1,7 @@
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 class CubitScreen extends StatelessWidget {
@@ -6,13 +9,27 @@ class CubitScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final newUsername = context.watch<UsernameCubit>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: Text(newUsername.state),
       ),
+      floatingActionButton: 
+        FloatingActionButton(
+          onPressed: (){
+
+            newUsername.setUsername(
+              RandomGenerator.getRandomName()
+            );
+
+          }, 
+          child: const Icon(Icons.refresh,),
+        )
     );
   }
 }
